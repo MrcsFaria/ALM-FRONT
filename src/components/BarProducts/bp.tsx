0793cc6b4bf0f66f47b5ac572a12212cfb7d10bb
MrcsFaria <contato.marcosvf@gmail.com>
2024-07-27 02:10:23 -0300

    update bp

--- src/components/BarProducts/bp.tsx
+++ src/components/BarProducts/bp.tsx
@@ -32,11 +32,11 @@ const FaixaSuperiorProducts: React.FC<FaixaSuperiorProductsProps> = ({ cartItems
     if (storedProducts) {
       try {
         const productArray: ProductWithQuantity[] = JSON.parse(storedProducts);
-  
+
         // Consolidar itens com o mesmo nome e tamanho
         const consolidatedProducts: ProductWithQuantity[] = productArray.reduce((accumulator: ProductWithQuantity[], item: ProductWithQuantity) => {
           const existingItem = accumulator.find(p => p.nome === item.nome && p.tamanhos === item.tamanhos);
-  
+
           if (existingItem) {
             // Atualiza a quantidade se o item já existir
             existingItem.quantidade += item.quantidade;
@@ -44,10 +44,10 @@ const FaixaSuperiorProducts: React.FC<FaixaSuperiorProductsProps> = ({ cartItems
             // Adiciona novo item à lista
             accumulator.push(item);
           }
-  
+
           return accumulator;
         }, []);
-  
+
         // Atualiza o estado com a lista consolidada
         setCartItems(consolidatedProducts);
       } catch (error) {
@@ -110,65 +110,69 @@ const FaixaSuperiorProducts: React.FC<FaixaSuperiorProductsProps> = ({ cartItems
 
   return (
     <div className="faixa-superior">
-      <Link to={`/`} style={{ textDecoration: 'none', color: 'inherit' }}>
-        <img
-          src={imagem1}
-          alt="Logo"
-          className="img-fluid mx-3"
-          style={{ height: '60px' }}
-        />
-      </Link>
-      <nav>
-        <a onClick={toggleCartDetails}>
-          <div className="cart-icon-container relative">
-            <AiOutlineShoppingCart className="nav-icons" style={{ color: 'white', cursor: 'pointer' }} />
-            {cartItemss.length > 0 && (
-              <span className="absolute -top-1 -right-1 bg-red-500 text-white text-sm rounded-full flex justify-center items-center w-5 h-5" style={{ cursor: 'pointer' }}>
-                {cartItemss.length}
+      <div className="faixa-superior-imagem">
+        <Link to={`/`} style={{ textDecoration: 'none', color: 'inherit' }}>
+          <img
+            src={imagem1}
+            alt="Logo"
+            className="img-fluid mx-3"
+            style={{ height: '60px' }}
+          />
+        </Link>
+      </div>
+      <div className="faixa-superior-navegacao">
+        <nav>
+          <a onClick={toggleCartDetails}>
+            <div className="cart-icon-container relative">
+              <AiOutlineShoppingCart className="nav-icons" style={{ color: 'white', cursor: 'pointer' }} />
+              {cartItemss.length > 0 && (
+                <span className="absolute -top-1 -right-1 bg-red-500 text-white text-sm rounded-full flex justify-center items-center w-5 h-5" style={{ cursor: 'pointer' }}>
+                  {cartItemss.length}
+                </span>
+              )}
+            </div>
+          </a>
+          {userName ? (
+            <div className='user-div'>
+              <FaRegUser style={{ color: 'white' }} />
+              <span className="usuario" style={{ color: 'white' }}>
+                {userName}
               </span>
-            )}
-          </div>
-        </a>
-        {userName ? (
-          <div className='user-div'>
-            <FaRegUser style={{ color: 'white' }} />
-            <span className="usuario" style={{ color: 'white' }}>
-              {userName}
-            </span>
-            <div>
-              <CiLogout style={{ color: 'white', height: '25px', width: '25px', cursor: 'pointer' }} onClick={handleLogout} />
+              <div>
+                <CiLogout style={{ color: 'white', height: '25px', width: '25px', cursor: 'pointer' }} onClick={handleLogout} />
+              </div>
+            </div>
+          ) : (
+            <Link to={`/SignUp`} style={{ textDecoration: 'none', color: 'inherit' }}>
+              <AiOutlineUserAdd className="nav-icons" style={{ color: 'white', cursor: 'pointer' }} />
+            </Link>
+          )}
+        </nav>
+        {showCartDetails && (
+          <div className={`fixed top-0 right-0 z-50 bg-black shadow-2xl w-90 h-full grid grid-rows-[60px_1fr_60px] transform transition-transform duration-500`}>
+            <h2 className='p-5 text-white text-2xl'>Carrinho</h2>
+            <div className='p-5 text-white'>
+              <ul style={{ listStyleType: 'none', padding: 0 }}>
+                {cartItemss.map((item, index) => (
+                  <li key={`${item.id}-${item.tamanhos}`} style={{ marginBottom: '10px', display: 'flex', alignItems: 'center' }}>
+                    {item.imagem && (
+                      <img src={getImageUrl(item.imagem.split(',')[0])} alt={item.nome} style={{ width: '50px', height: '50px', marginRight: '10px' }} />
+                    )}
+                    <span style={{ marginRight: '10px' }}>{item.nome}</span> - Tamanho: {item.tamanhos} - QTD: {item.quantidade} - R$ {(item.preco * item.quantidade).toFixed(2)}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className='grid grid-cols-2'>
+              <button className='bg-secondary text-white' onClick={toggleCartDetails}>Fechar</button>
+              <button className='bg-amber-600 text-white' onClick={handleCheckoutClick}>
+                Checkout
+              </button>
             </div>
           </div>
-        ) : (
-          <Link to={`/SignUp`} style={{ textDecoration: 'none', color: 'inherit' }}>
-            <AiOutlineUserAdd className="nav-icons" style={{ color: 'white', cursor: 'pointer' }} />
-          </Link>
         )}
-      </nav>
+      </div>
 
-      {showCartDetails && (
-        <div className={`fixed top-0 right-0 z-50 bg-black shadow-2xl w-90 h-full grid grid-rows-[60px_1fr_60px] transform transition-transform duration-500`}>
-          <h2 className='p-5 text-white text-2xl'>Carrinho</h2>
-          <div className='p-5 text-white'>
-            <ul style={{ listStyleType: 'none', padding: 0 }}>
-              {cartItemss.map((item, index) => (
-                <li key={`${item.id}-${item.tamanhos}`} style={{ marginBottom: '10px', display: 'flex', alignItems: 'center' }}>
-                  {item.imagem && (
-                    <img src={getImageUrl(item.imagem.split(',')[0])} alt={item.nome} style={{ width: '50px', height: '50px', marginRight: '10px' }} />
-                  )}
-                  <span style={{ marginRight: '10px' }}>{item.nome}</span> - Tamanho: {item.tamanhos} - QTD: {item.quantidade} - R$ {(item.preco * item.quantidade).toFixed(2)}
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className='grid grid-cols-2'>
-            <button className='bg-secondary text-white' onClick={toggleCartDetails}>Fechar</button>
-            <button className='bg-amber-600 text-white' onClick={handleCheckoutClick}>
-              Checkout
-            </button>
-          </div>
-        </div>
-      )}
     </div>
   );
 }
